8cd9ad58b5d1c6ea2f6e283b0333a9c7ba8dc98c
Alessandro Scotti <alex@rails.(none)>
2012-01-15 18:49:37 +0100

    fix

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,11 +1,8 @@
-require 'feed'
-
 class SubjectsController < ApplicationController
 
 # GET /subjects
   # GET /subjects.json
   def index
-    feed = Feed.new
     @subjects = Subject.grouped
 
     respond_to do |format|
