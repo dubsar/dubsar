2070f81ac401b6d57df2d0c669ebed269b0a12ac
Alessandro Scotti <scotti.alessandro@gmail.com>
2012-02-11 19:15:46 +0100

    Removed Schema From Heroku Defined Functions

--- db/sql/heroku-functions.sql
+++ db/sql/heroku-functions.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE FUNCTION dubsar.add_to_searches(INT, TEXT, TEXT, TEXT) RETURNS INTEGER  AS $$
-	INSERT INTO dubsar.searches(item_id, item_relation, description, content) VALUES (
+	INSERT INTO searches(item_id, item_relation, description, content) VALUES (
 		$1,
 		$2,
 		substr(($3), 0, 100), 
@@ -10,7 +10,7 @@ CREATE OR REPLACE FUNCTION dubsar.add_to_searches(INT, TEXT, TEXT, TEXT) RETURNS
 
 CREATE OR REPLACE RULE youtube_videos_search AS 
 	ON INSERT
-	TO dubsar.youtube_videos
+	TO youtube_videos
 	DO SELECT dubsar.add_to_searches(
 		new.id - 1,
 		'youtube_videos',
