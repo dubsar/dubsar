39779b2e4f71e9f083d10310210c54c9c7d6b187
Alessandro Scotti <scotti.alessandro@gmail.com>
2012-04-12 22:06:19 +0200

    ResourceController code cleanup

--- lib/dubsar/resource_controller.rb
+++ lib/dubsar/resource_controller.rb
@@ -1,21 +1,17 @@
 class ResourceController
-  def initialize(namespace, matter)
-    @namespace = namespace.to_s.camelize
+  def initialize(context, matter)
+    @context = context.to_s.camelize
     @matter = matter
   end
   def create
-    create_controller(@matter)
+    create_controller
   end
   private
-  def create_controller(matter)
-    #puts matter
-    return if exists?(matter)
-    klass_name = name(matter.name)
+  def create_controller
+    return if exists?
     klass = Class.new(Matters::MattersController)
-    namespace.constantize.const_set(klass_name, klass)
-    model_klass_name = model_name(matter.name)
-    model_klass = "#{namespace}::#{model_klass_name}".constantize
-    namespace.constantize.const_get(klass_name).class_eval do
+    namespace.const_set(name, klass)
+    namespace.const_get(name).class_eval do
       layout 'home'
       def index
         respond_with(@matter = model_klass.all)
@@ -27,23 +23,20 @@ class ResourceController
         respond_with(@matter = model_klass.new)
       end
       def create
-        @matter = model_klass.create(find)
+        @matter = model_klass.create(find_form)
         respond_with(@matter)
       end
       private
       def model_klass
-        name = self.class.name.gsub(/(.*)(Controller)/, '\1').singularize
-        name.constantize
+        model_name = self.class.name.sub(/(.*)(Controller)/, '\1').singularize
+        model_name.constantize
       end
-      def find
-        p = params[params.keys.select {|k| k.start_with?("matters")}.first]
-        puts p.inspect
-        p
+      def find_form
+        params[params.keys.select {|k| k.start_with?("matters")}.first]
       end
     end
   end
-  def exists?(matter)
-    name = name(matter.name)
+  def exists?
     begin
       eval "#{namespace}.const_get('#{name}')"
     rescue
@@ -52,12 +45,9 @@ class ResourceController
     true
   end
   def namespace
-    "Matters::" << @namespace
+    ("Matters::" << @context).constantize
   end
-  def name(klass)
-    klass.camelize << "Controller"
-  end
-  def model_name(klass)
-    klass.singularize.camelize
+  def name
+    @matter.name.camelize << "Controller"
   end
 end
